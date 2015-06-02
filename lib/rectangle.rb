b92487d7ba0c6dd0ba4dbd1ce400007948d5877d
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-02 18:22:05 +0530

    Obtain Diagonal points instead of length and breadth for creating rectangle

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,8 +1,9 @@
 # Defines attributes of a Rectangle 
 class Rectangle
-	def initialize(length,breadth)
-		@length = length
-		@breadth = breadth
+	
+	def initialize(diagonal_point1,diagonal_point2)
+		@length = (diagonal_point1.x - diagonal_point2.x).abs
+		@breadth = (diagonal_point1.y - diagonal_point2.y).abs
 	end
 
 	def perimeter
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,24 +1,60 @@
 require 'spec_helper'
 	
 describe Rectangle do
-	it "given a length of 3 units and a breadth of 2 units it should return the perimeter as 10 units" do
-		rectangle=Rectangle.new(3,2)
-		expect(rectangle.perimeter).to eq(10)
+	it "given two points with coordinates(2,3) and (3,5) it should return the perimeter as 6 units" do
+		point1 = Point.new(2,3)
+		point2 = Point.new(3,5)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.perimeter).to eq(6)
 	end
 	
-	it "given a length of 2 units and a breadth of 4 units it should return the perimeter as 10 units" do
-		rectangle=Rectangle.new(2,4)
+	it "given two points with coordinates(5,5) and (3,3) it should return the perimeter as 8 units" do
+		point1 = Point.new(5,5)
+		point2 = Point.new(3,3)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.perimeter).to eq(8)
+	end
+
+	it "given two points with coordinates(5,3) and (3,7) it should return the perimeter as 12 units" do
+		point1 = Point.new(5,3)
+		point2 = Point.new(3,7)
+		rectangle=Rectangle.new(point1,point2)
 		expect(rectangle.perimeter).to eq(12)
 	end
 
-	it "given a length of 20 units and a breadth of 5 units it should return the perimeter as 10 units" do
-		rectangle=Rectangle.new(20,5)
-		expect(rectangle.area).to eq(100)
+	it "given two points with coordinates(1,1) and (-1,-3) it should return the perimeter as 12 units" do
+		point1 = Point.new(1,1)
+		point2 = Point.new(-1,-3)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.perimeter).to eq(12)
 	end
 
-	it "given a length of 10 units and a breadth of 15 units it should return the perimeter as 10 units" do
-		rectangle=Rectangle.new(10,15)
-		expect(rectangle.area).to eq(150)
+	it "given two points with coordinates(1,1) and (-1,-3) it should return the area as 8 square units" do
+		point1 = Point.new(1,1)
+		point2 = Point.new(-1,-3)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.area).to eq(8)
+	end
+
+	it "given two points with coordinates(5,3) and (3,7) it should return the area as 8 square units" do
+		point1 = Point.new(5,3)
+		point2 = Point.new(3,7)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.perimeter).to eq(8)
+	end
+
+	it "given two points with coordinates(-5,5) and (3,-3) it should return the area as 64 square units" do
+		point1 = Point.new(-5,5)
+		point2 = Point.new(3,-3)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.area).to eq(64)
+	end
+
+	it "given two points with coordinates(2,5) and (-3,4) it should return the area as 5 square units" do
+		point1 = Point.new(2,5)
+		point2 = Point.new(-3,4)
+		rectangle=Rectangle.new(point1,point2)
+		expect(rectangle.area).to eq(5)
 	end
 
 end
