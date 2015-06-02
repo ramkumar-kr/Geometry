1818b6679ef4f53ad12085083ae98e3694e38433
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-02 16:17:56 +0530

    Added missing test cases

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -26,6 +26,16 @@ describe Line do
 		expect(line_1).to eq(line_2)
  	end
 
+ 	it "having end points with values (1,2) and (2,3) should be equal to another line having (2,3) and (1,2) as end points" do
+		line_1_end_point_1 = Point.new(1,2)
+		line_1_end_point_2 = Point.new(2,3)
+		line_2_end_point_1 = Point.new(2,3)
+		line_2_end_point_2 = Point.new(1,2)
+		line_1 = Line.new(line_1_end_point_1, line_1_end_point_2)
+		line_2 = Line.new(line_2_end_point_1, line_2_end_point_2)
+		expect(line_1).to eq(line_2)
+ 	end
+
 	it "having end points with values (1,2) and (4,3) should not be equal to another line having (1,2) and (2,3) as end points" do
 		line_1_end_point_1 = Point.new(1,2)
 		line_1_end_point_2 = Point.new(4,3)
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -20,6 +20,12 @@ describe Point do
 		expect(point1).to_not eq(point2)
 	end
 
+	it "having coordinates (2,3) should have a distance of 5 with another point of distance (5,7)" do
+		point1 = Point.new(2,3)
+		point2 = Point.new(5,7)
+		expect(point1.distance(point2)).to eq(5)
+	end
+
 	it "having coordinates(4,3) should be equal to itself" do
 		point = Point.new(4,3)
 		expect(point).to eq(point)
@@ -33,14 +39,14 @@ describe Point do
 	it "comparision of hash of two objects with same data yield the same hash" do
 		point1 = Point.new(4,3)
 		point2 = Point.new(4,3)
-
 		expect(point1.hash).to eq(point2.hash)
 	end
 
-	it "hash" do
+	it "comparision of hash of two objects with different data do not yield the same hash" do
 		point1 = Point.new(4,3)
 		point2 = Point.new(3,4)
-
 		expect(point1.hash).to_not eq(point2.hash)
 	end
+
+	
 end
